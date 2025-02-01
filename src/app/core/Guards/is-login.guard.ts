d0c9721b659337e31c102ed5bff11331264a26ed
Mayar-Fawzy <momayar920@gmail.com>
2025-02-02 01:42:05 +0200

    ServiesDone

--- src/app/core/Guards/is-login.guard.ts
+++ src/app/core/Guards/is-login.guard.ts
@@ -8,7 +8,7 @@ export const isLoginGuard: CanActivateFn = (route, state) => {
   const _PLATFORM_ID = inject(PLATFORM_ID);
   if (isPlatformBrowser(_PLATFORM_ID)) {
     const _Router = inject(Router);
-    if (localStorage.getItem('UserAuth') !== null) {  
+    if (localStorage.getItem('User') !== null) {  
       _Router.navigate(['/home']);
       return false;
   }
